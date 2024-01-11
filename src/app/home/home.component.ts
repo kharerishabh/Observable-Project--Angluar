b93e2c238f62d69d981690431e35165a7fea39ce
KHARERISHABH <rishabkhare41@gmail.com>
2024-01-11 16:40:52 +0530

    creating the custom count  observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
+import { Observable, Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +12,16 @@ export class HomeComponent implements OnInit, OnDestroy{
   constructor() { }
 
   ngOnInit() {
-   this.firstSubscription = interval(1000).subscribe(count => console.log(count))
+   //this.firstSubscription = interval(1000).subscribe(count => console.log(count))
+    //Custom Observable
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0
+      setInterval(() => {
+        observer.next(count)
+        count++
+      }, 1000)
+    })
+    this.firstSubscription = customIntervalObservable.subscribe(data => console.log(data))
   }
   ngOnDestroy(): void {
       this.firstSubscription.unsubscribe()
