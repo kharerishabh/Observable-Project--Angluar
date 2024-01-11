217a4fc28e63ab8d43b05bd9b96eed770939fa79
KHARERISHABH <rishabkhare41@gmail.com>
2024-01-11 21:03:24 +0530

    knowing the observer method and use of them next, error, completion methods

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,26 +4,42 @@ import { Observable, Subscription, interval } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit, OnDestroy{
+export class HomeComponent implements OnInit, OnDestroy {
   private firstSubscription: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-   //this.firstSubscription = interval(1000).subscribe(count => console.log(count))
+    //this.firstSubscription = interval(1000).subscribe(count => console.log(count))
     //Custom Observable
-    const customIntervalObservable = Observable.create(observer => {
-      let count = 0
+    const customIntervalObservable = Observable.create((observer) => {
+      let count = 0;
       setInterval(() => {
-        observer.next(count)
-        count++
-      }, 1000)
-    })
-    this.firstSubscription = customIntervalObservable.subscribe(data => console.log(data))
+        observer.next(count);
+        if(count === 3){
+          observer.complete()
+        }
+        if (count > 4) {
+          observer.error(new Error('Count is greater than 4'));
+        }
+        count++;
+      }, 1000);
+    });
+    this.firstSubscription = customIntervalObservable.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        alert(error.message);
+      },
+      () => {
+        console.log('Completed')
+      }
+    );
   }
   ngOnDestroy(): void {
-      this.firstSubscription.unsubscribe()
+    this.firstSubscription.unsubscribe();
   }
 }
